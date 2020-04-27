1a3b83213e5891937c3b4c8d2c5396fa535920bb
Trapti Rahangdale <imtrapti1410@gmail.com>
2020-04-27 22:44:01 +0530

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import { Router } from "components/Router";
 import "./app.css";
 import Logo from "./components/logo/logo";
 
+function scrollTop() {
+  return window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
 function App() {
   return (
     <Root>
@@ -19,6 +27,9 @@ function App() {
           </Router>
         </React.Suspense>
       </main>
+      <div className="scroll-to-top">
+        <a onClick={() => scrollTop()}>^</a>
+      </div>
     </Root>
   );
 }
